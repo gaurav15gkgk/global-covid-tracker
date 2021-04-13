f1f82e437e71050cd40f65d42c8d354c2a7b623f
You know me <hiyouknowme1515@gmail.com>
2021-04-14 00:48:11 +0530

    card component of showing info of infected, recoveries, deaths is done

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Typography , Grid } from '@material-ui/core'
+import CardComponent from './Card/Card'
+import styles from './Cards.module.css'
 
-const Cards = () => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
+    if(!confirmed){
+        return 'Data is loading please wait'
+    }
+
     return (
-        <div>
-            Cards
+        <div className={styles.container }>
+            <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+            <Grid container spacing={3} justify="center"> 
+                <CardComponent 
+                    className={styles.infected}
+                    cardTitle="Infected"
+                    value={confirmed.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of active cases from COVID-19 pandemic"
+                />
+                <CardComponent 
+                    className={styles.recovered}
+                    cardTitle="Recovered"
+                    value={recovered.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of recoveries from COVID-19 pandemic"
+                />
+                <CardComponent 
+                    className={styles.death}
+                    cardTitle="Deaths"
+                    value={deaths.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of deaths from COVID-19 pandemic"
+                />
+            </Grid>
         </div>
     )
 }
