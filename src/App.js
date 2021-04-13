a85a84c3675ae5737a9255fac0ed278d991526fb
You know me <hiyouknowme1515@gmail.com>
2021-04-13 21:06:56 +0530

    Initial setup done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+ 
+ import { Cards, CountryPicker, Chart } from './Components'
+
+function App() {
+  return (
+    <>
+          <h1>This is covid tracker app</h1>
+          <Cards />
+          <Chart />
+          <CountryPicker />
+    </>
+ 
+  )
+}
+
+export default App;
